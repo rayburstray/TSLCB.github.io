c8bad60e7a2dc2271c2d8dd8aa8ac2e4fb2119d1
rayburstray <3597468903@qq.com>
2025-03-03 00:17:48 +0800

    Site updated: 2025-03-03 00:17:47

--- javascripts/post-info-button.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("post-info-button").addEventListener("click", function(){
-  alert("Under construction.");
-});
--- javascripts/waiter.js
+++
@@ -1,7 +0,0 @@
-// var contentContainer = document.getElementById("content-container");
-let loader = document.querySelector(".site-drawer__loading-tip");
-
-window.onload = function() {
-  // contentContainer.style.overflow = "auto";
-  loader.style.display = "none";
-}
